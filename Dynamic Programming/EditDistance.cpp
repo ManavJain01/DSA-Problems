913f1291b218aeebbefdb39ee6325181439598cb
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-03-18 23:53:25 +0530

    commit

---
+++ Dynamic Programming/EditDistance.cpp
@@ -0,0 +1,35 @@
+// 72. Edit Distance
+// https://leetcode.com/problems/edit-distance/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int f(int i, int j, string &s1, string& s2, vector<vector<int>>& dp){
+      if(i<0) return j+1;
+      if(j<0) return i+1;
+      if(dp[i][j] != -1) return dp[i][j];
+      if(s1[i] == s2[j]) return dp[i][j] = f(i-1, j-1, s1, s2, dp);
+      return dp[i][j] = 1 + min(f(i-1, j, s1, s2, dp), min(f(i, j-1, s1, s2, dp), f(i-1, j-1, s1, s2, dp)));
+    }
+    int minDistance(string word1, string word2) {
+      int n = word1.size();
+      int m = word2.size();
+      vector<vector<int>>dp(n, vector<int>(m, -1));
+      return f(n-1, m-1, word1, word2, dp);
+    }
+};
+
+int main(){
+  string str1 = "horse";
+  string str2 = "ros";
+  Solution s;
+  cout<<endl<<endl;
+  int num = s.minDistance(str1, str2);
+  
+  cout<<endl<<num<<endl;
+  
+
+  return 0;
+}
